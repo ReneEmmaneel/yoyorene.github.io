7c2c7c6f1f9af75dede4572b6adeecb049424957
Rene Emmaneel <reneemmaneel00@gmail.com>
2021-06-27 17:31:56 +0200

    Added simple login system

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   get "/articles/new", to: "articles#new"
   get "/articles/:id", to: "articles#show"
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#delete'
+  get 'welcome', to: 'sessions#welcome'
+  get 'delete', to: 'sessions#delete'
+
+  get 'authorized', to: 'sessions#page_requires_login'
+
   resources :articles
+  resources :users, only: [:new, :create, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
